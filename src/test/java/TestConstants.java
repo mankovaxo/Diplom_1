f20674d63fbc0eab9a8aee9f07fd464d888d681b
csenotik <vladda2000@yandex.ru>
2025-09-18 14:13:17 +0300

    First commit of Diplom_1

---
+++ src/test/java/TestConstants.java
@@ -0,0 +1,18 @@
+package praktikum;
+
+public class TestConstants {
+
+    public static final String BLACK_BUN_NAME = "black bun";
+    public static final float BUN_PRICE = 100f;
+
+    public static final String HOT_SAUCE_NAME = "hot sauce";
+    public static final String CHILI_SAUCE_NAME = "chili sauce";
+    public static final float SAUCE_PRICE = 100f;
+    public static final float CHILI_SAUCE_PRICE = 300f;
+
+    public static final String CUTLET_NAME = "cutlet";
+    public static final float FILLING_PRICE = 100f;
+
+    public static final String RECEIPT_STRUCTURE = "(==== %s ====)%n= %s %s =%n(==== %s ====)%n%nPrice: %f%n";
+
+}
